abe5183ed0099ac1470d8860e8cbff5e7e9e0cc4
Quinn Shannon <quinn.p.shannon@gmail.com>
2024-05-17 09:24:56 -0400

    Made some late-night progress

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 9876;
 
 app.use(express.json());
-app.options('*',cors());
+app.use(cors());
 app.use('/api', taskRouter);
 app.use('/users', userRouter);
 
@@ -19,26 +19,30 @@ app.use((req,res,next) =>{
   next();
 });
 app.get('/card',(req,res) =>{
+  const randCard = [];
   const values = [];
   for(let x=0;x<9;x++){
     let id;
     do{
-      id=Math.floor(Math.random() * taskList.length);
+      id=Math.floor(Math.random() * taskList.length+1);
     }
     while(values.includes(id))
-      values.push(id);
+    values.push(id);
+    randCard.push(taskList.find(task => task.id === id))
+    randCard[x].completed = false;
   }
-  res.send(pug.renderFile('./pug/card.pug', {
-    task1:taskList[values[0]].title,
-    task2:taskList[values[1]].title,
-    task3:taskList[values[2]].title,
-    task4:taskList[values[3]].title,
-    task5:taskList[values[4]].title,
-    task6:taskList[values[5]].title,
-    task7:taskList[values[6]].title,
-    task8:taskList[values[7]].title,
-    task9:taskList[values[8]].title 
-  })).status(200);
+  res.json(randCard).status(200);
+  // res.send(pug.renderFile('./pug/card.pug', {
+  //   task1:taskList[values[0]].title,
+  //   task2:taskList[values[1]].title,
+  //   task3:taskList[values[2]].title,
+  //   task4:taskList[values[3]].title,
+  //   task5:taskList[values[4]].title,
+  //   task6:taskList[values[5]].title,
+  //   task7:taskList[values[6]].title,
+  //   task8:taskList[values[7]].title,
+  //   task9:taskList[values[8]].title 
+  // })).status(200);
 });
 
 app.listen(port, () => {
--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 import tasks from "../data/tasks.js";
-
 const play = document.getElementById('main');
 const nav = document.getElementById('topNav')
-// const baseURL = 'http://localhost:9876'
-console.log(tasks);
+const baseURL = 'http://localhost:9876'
+const card = await getCard();
+buildHome();
 
 function playClick(e){
     if(e.target.nodeName != 'DIV') return;
     if(!e.target.classList.contains('complete')){
         e.target.classList.toggle('complete');
+        card[Number(e.target.id.slice(-1))-1].completed = true;
+
     }
-    console.log(e.target.id)
 }
 function navClick(e){
     if(e.target.nodeName != 'LI') return;
@@ -22,20 +23,63 @@ function navClick(e){
         case 'card':
             buildBingo();
             break;
-        case 'suggestion':
-            // buildSuggest();
+        case 'details':
+            buildDetails();
             break;
     }
-    if(!e.target.classList.contains('complete')){
-        e.target.classList.toggle('complete');
-    }
-    console.log(e.target.nodeName)
 }
 function clearPlay(){
     while(play.lastElementChild) play.lastElementChild.remove();
 }
-function buildSuggest(){
-
+function buildHome(){
+    const newForm = document.createElement('form');
+    newForm.style.width = '300px';
+    const userLabel = document.createElement('label');
+    userLabel.classList.add('row')
+    userLabel.style.marginLeft = 'auto'
+    userLabel.innerText = "User Name";
+    const userName = document.createElement('input');
+    const passLabel = document.createElement('label');
+    passLabel.classList.add('row')
+    passLabel.style.marginLeft = 'auto'
+    passLabel.innerText = "Password";
+    const password = document.createElement('input');
+    password.setAttribute('type','password')
+    const div = document.createElement('div');
+    div.classList.add('row')
+    div.style.justifyContent = 'center';
+    const submit = document.createElement('button');
+    submit.innerText = "SaveCard";
+    submit.style.marginLeft = 'auto';
+    submit.addEventListener('click',(e)=>{
+        e.preventDefault();
+        buttonClick(e,userName.value,password.value)
+    });
+    userLabel.appendChild(userName);
+    newForm.appendChild(userLabel);
+    passLabel.appendChild(password);
+    newForm.appendChild(passLabel);
+    div.appendChild(submit);
+    newForm.appendChild(div);
+    play.appendChild(newForm);
+}
+function buildDetails(){
+    if(!play.classList.contains("column")) play.classList.toggle("column");
+    for(let x=0;x<9;x++){
+        const column = document.createElement('ul');
+        column.classList.add('column')
+        x%2 === 0 ? column.style.background = 'var(--Hydro)' : column.style.background = 'var(--Pyro)';
+        if (card[x].completed) continue;
+        const title = document.createElement('li')
+        title.classList.add('row')
+        title.innerText = card[x].title;
+        const desc = document.createElement('li')
+        desc.classList.add('row')
+        desc.innerText = card[x].description;
+        column.appendChild(title);
+        column.appendChild(desc);
+        play.appendChild(column);
+    }
 }
 function buildBingo(){
     for(let x=0;x<3;x++){
@@ -45,13 +89,24 @@ function buildBingo(){
             const bingo = document.createElement('div')
             const ID = `t${(x*3)+y}`;
             bingo.setAttribute('id',ID)
-            bingo.innerText = `Row ${x+1} Box ${y+1}`;
-            console.log(ID);
+            bingo.style.width = '180px'
+            bingo.innerText = card[((x*3)+y-1)].title;
+            if(card[((x*3)+y-1)].completed) bingo.classList.toggle('complete');
             row.appendChild(bingo);
         }
         play.appendChild(row);
     }
 }
-
+async function getCard(){
+    const result = await fetch(baseURL+'/card');
+    const newCard = await result.json();
+    return newCard;
+}
+async function buttonClick(e,usr,pass){
+    
+    console.log(e);
+    console.log(usr);
+    console.log(pass);
+}
 play.addEventListener('click',playClick)
 nav.addEventListener('click',navClick)
